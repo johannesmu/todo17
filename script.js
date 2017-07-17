b8f57ea27b09568a32c5f79a6b1470039c3ccab7
johannes <jmuljana@gmail.com>
2017-07-17 18:54:12 +1000

    updated classes to change template logic

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ let appconfig = {
   listviewid:'task-list',
   templateid:'task-template'
 }
-//task
-class Task{
-  constructor(name){
-    this.date = new Date().getTime();
-    this.name = name;
-    this.status = 0;
+
+//template
+class TaskTemplate{
+  constructor(templateid){
+    this.template = document.querySelector('#'+templateid);
+    this.item = this.template.querySelector('li');
+    this.status = this.item.getAttribute('data-status');
+    this.id = this.item.getAttribute('id');
     return this;
   }
 }
-//template
-class TaskTemplate{
-  constructor(templateid,obj){
-    this.template = document.querySelector('#'+templateid);
-    this.item = this.template.querySelector('li');
-    //this.status = this.item.getAttribute('data-status');
-    return this.item;
+
+//task
+class Task{
+  constructor(name){
+    this.id = new Date().getTime();
+    this.name = name;
+    this.status = 0;
+  }
+  template(){
+    this.template = new TaskTemplate('task-template');
+    this.template.id = this.id;
+    this.template.status = this.status;
+    //clone the template and return the element
+    let clone = document.importNode(t.content, true);
   }
 }
 
@@ -37,8 +46,6 @@ class List{
     let i=0;
     for(i=0;i<this.count;i++){
       let item = this.list[i];
-      let lt = new TaskTemplate(appconfig.templateid,item);
-      
       
     }
   }
